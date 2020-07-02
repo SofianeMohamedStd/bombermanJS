a0dcb0ab6a1b3fd7ba747bfa7b05589fdd6ff3b1
SofinaeMohamedStd <namoune.sofianemohamed@gmail.com>
2020-07-02 16:53:12 +0200

    commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,7 +4,7 @@ let zonejeu = document.querySelector("#zonejeu");
 // le joueur
 let player = document.querySelector("#joueur");
 
-const ennemies = [].slice.call(document.querySelectorAll('.ennemi'));
+
 
 
 //mur
@@ -48,14 +48,14 @@ function move(p, d) {
 
     switch (d) {
         case "up":
-            
 
-            Boolean=false;
-            for(let i = 0; i<murs.length; i++){
+
+            Boolean = false;
+            for (let i = 0; i < murs.length; i++) {
                 let topMur = getProperty(murs[i], "top");
-                let leftMur = getProperty(murs[i],"left");
-                
-                if(topMur + 50 == topElement && leftMur == leftElement){
+                let leftMur = getProperty(murs[i], "left");
+
+                if (topMur + 50 == topElement && leftMur == leftElement) {
                     Boolean = true;
                     break;
                 }
@@ -63,26 +63,26 @@ function move(p, d) {
             if (topElement > 0 && Boolean == false) {
                 p.style.top = topElement - 50 + "px";
             }
-           
+
             else {
                 audioElement.play()
             }
             break;
 
         case "down":
-            
 
-            Boolean=false;
-            for(let i = 0; i<murs.length; i++){
+
+            Boolean = false;
+            for (let i = 0; i < murs.length; i++) {
                 let topMur = getProperty(murs[i], "top");
-                let leftMur = getProperty(murs[i],"left");
-                
-                if(topMur == topElement + 50 && leftMur == leftElement){
+                let leftMur = getProperty(murs[i], "left");
+
+                if (topMur == topElement + 50 && leftMur == leftElement) {
                     Boolean = true;
                     break;
                 }
             };
-            if (topElement < 600 && Boolean == false ) {
+            if (topElement < 600 && Boolean == false) {
                 p.style.top = topElement + 50 + "px";
             }
             else {
@@ -91,12 +91,12 @@ function move(p, d) {
             break;
 
         case "left":
-            Boolean=false;
-            for(let i = 0; i<murs.length; i++){
+            Boolean = false;
+            for (let i = 0; i < murs.length; i++) {
                 let topMur = getProperty(murs[i], "top");
-                let leftMur = getProperty(murs[i],"left");
-                
-                if(topMur == topElement  && leftMur +50 == leftElement){
+                let leftMur = getProperty(murs[i], "left");
+
+                if (topMur == topElement && leftMur + 50 == leftElement) {
                     Boolean = true;
                     break;
                 }
@@ -110,12 +110,12 @@ function move(p, d) {
             break;
 
         case "right":
-            Boolean=false;
-            for(let i = 0; i<murs.length; i++){
+            Boolean = false;
+            for (let i = 0; i < murs.length; i++) {
                 let topMur = getProperty(murs[i], "top");
-                let leftMur = getProperty(murs[i],"left");
-                
-                if(topMur == topElement  && leftMur == leftElement + 50){
+                let leftMur = getProperty(murs[i], "left");
+
+                if (topMur == topElement && leftMur == leftElement + 50) {
                     Boolean = true;
                     break;
                 }
@@ -129,6 +129,8 @@ function move(p, d) {
             break;
     }
     PlayerEnnemi();
+    takebonus();
+
 }
 
 //deplacement de l'ennemi
@@ -136,36 +138,75 @@ function move1(en, d) {
     const leftElement = getProperty(en, "left");
     const topElement = getProperty(en, "top");
 
-        
+
 
     switch (d) {
         case "up":
-            if (topElement > 0 ) {
+            Boolean = false;
+            for (let i = 0; i < murs.length; i++) {
+                let topMur = getProperty(murs[i], "top");
+                let leftMur = getProperty(murs[i], "left");
+
+                if (topMur + 50 == topElement && leftMur == leftElement) {
+                    Boolean = true;
+                    break;
+                }
+            };
+            if (topElement > 0 && Boolean == false) {
                 en.style.top = topElement - 50 + "px";
             }
             break;
 
         case "down":
-            if (topElement < 600) {
+            Boolean = false;
+            for (let i = 0; i < murs.length; i++) {
+                let topMur = getProperty(murs[i], "top");
+                let leftMur = getProperty(murs[i], "left");
+
+                if (topMur == topElement + 50 && leftMur == leftElement) {
+                    Boolean = true;
+                    break;
+                }
+            };
+            if (topElement < 600 && Boolean == false) {
                 en.style.top = topElement + 50 + "px";
             }
             break;
 
         case "left":
-            if (leftElement > 0 ) {
+            Boolean = false;
+            for (let i = 0; i < murs.length; i++) {
+                let topMur = getProperty(murs[i], "top");
+                let leftMur = getProperty(murs[i], "left");
+
+                if (topMur == topElement && leftMur + 50 == leftElement) {
+                    Boolean = true;
+                    break;
+                }
+            };
+            if (leftElement > 0 && Boolean == false) {
                 en.style.left = leftElement - 50 + "px";
             }
             break;
 
         case "right":
-            if (leftElement < 600 ) {
+            for (let i = 0; i < murs.length; i++) {
+                let topMur = getProperty(murs[i], "top");
+                let leftMur = getProperty(murs[i], "left");
+
+                if (topMur == topElement && leftMur == leftElement + 50) {
+                    Boolean = true;
+                    break;
+                }
+            };
+            if (leftElement < 600 && Boolean == false) {
                 en.style.left = leftElement + 50 + "px";
             }
             break;
 
-            
+
     }
-    
+
 }
 // ecouter la frappe des fleches et appliquer la fonction move
 window.addEventListener("keydown", function (e) {
@@ -177,35 +218,24 @@ window.addEventListener("keydown", function (e) {
     let key_code = e.keyCode;
     switch (key_code) {
         case 81:
-            
-           
-                
-               move(player, "left");
-            
-
+            move(player, "left");
             break;
-
         case 68:
-           
-              move(player, "right");  
-            
+            move(player, "right");
             break;
-
         case 90:
-           
-               move(player, "up"); 
-            
+            move(player, "up");
             break;
-
         case 83:
-           
-                move(player, "down");
+            move(player, "down");
             break;
         case 32:
             createBomb(getProperty(player, "top"), getProperty(player, "left"));
             audioElement.play();
-
             break;
+        case 96:
+            document.location.reload(true);
+        break;    
     }
 
 });
@@ -218,7 +248,7 @@ function createBomb(top, left) {
     zonejeu.appendChild(bomb);
     setTimeout(function () {
         createExplosion(bomb);
-    }, 1000);
+    }, 3000);
 }
 
 //fonction  creation d'explosion et supprission de bomb
@@ -228,14 +258,14 @@ function createExplosion(bomb) {
     audioElement1.play();
     audioElement.play();
     exploseBomb(bomb);
-    
+
     setTimeout(function () {
         zonejeu.removeChild(bomb);
-    }, 500);
+    }, 800);
 }
 
 //fonction de creation d'ennemi
-/*for (let i = 0; i < getRandomizer(5, 2); i++) {
+for (let i = 0; i < 5; i++) {
     function createEnnemi(top, left) {
         let ennemi = document.createElement("div");
         ennemi.setAttribute("class", "ennemi");
@@ -245,46 +275,48 @@ function createExplosion(bomb) {
 
     }
 
-    window.addEventListener('load', createEnnemi(getRandomizer(500, 0), getRandomizer(500, 0)));
-}*/
+    window.addEventListener('load', createEnnemi(getRandomizer(12, 0) * 50, getRandomizer(12, 0) * 50));
+}
 
 // tableau des ennemis 
-let button = document.querySelector('button');
+
 //faire deplacer les énnemis aléatoirement
+//const ennemies = [].slice.call(document.querySelectorAll('.ennemi'));
+const ennemies = [... document.querySelectorAll('.ennemi')];
 setInterval(function () {
     for (let i = 0; i < ennemies.length; i++) {
         let enn = ennemies[i];
         let direction = directions[getRandomizer(3, 0)];
         move1(enn, direction);
-        
+
     }
-}, 800)
+}, 500)
 
 
-/*function Dennemis() {
-    setInterval(function(){
+/*let button = document.querySelector('button');
+button.addEventListener('click',() => setInterval(function(){
         for (let i = 0; i < ennemies.length; i++) {
             let enn = ennemies[i];
             let direction = directions[getRandomizer(3, 0)];
             move1(enn, direction);
         }
-    },800)
-}*/
+    },500));*/
 
 
-//button.addEventListener('click',Dennemis);
 
-function imlose(){
+
+
+function imlose() {
     let lose = document.createElement("div");
     lose.setAttribute("class", "imglose");
-    lose.style.top =  "350px";
+    lose.style.top = "350px";
     lose.style.left = "350px";
     zonejeu.appendChild(lose);
 }
-function imwin(){
+function imwin() {
     let win = document.createElement("div");
     win.setAttribute("class", "imgwin");
-    win.style.top =  "350px";
+    win.style.top = "350px";
     win.style.left = "350px";
     zonejeu.appendChild(win);
 }
@@ -303,40 +335,41 @@ function exploseBomb(bomb) {
 
             zonejeu.removeChild(enn);
             ennemies.splice(i, 1);
-            if(ennemies.length <= 0){
+            if (ennemies.length <= 0) {
                 imwin();
             }
 
         }
     }
 
-    const playerTop = getProperty(player,"top");
-    const playerLeft = getProperty(player,"left");
+    const playerTop = getProperty(player, "top");
+    const playerLeft = getProperty(player, "left");
 
-    if ((playerTop >= bombTop && playerTop <= bombTop + 100) && (playerLeft >= bombLeft && playerLeft <= bombLeft + 100)) {
+    if ((playerTop >= bombTop && playerTop <= bombTop + 150) && (playerLeft >= bombLeft && playerLeft <= bombLeft + 150)) {
 
         life--;
         if (life === 4) {
 
-            //faire blinker le player pendant 3sec
-            
-            vie.innerHTML = "attention que 4 vies";
-            
+
+
+            vie.innerHTML = " il vous reste 4 vies";
+
         }
-         if (life === 3) {
-            vie.innerHTML = "attention que 3 vies";
+        if (life === 3) {
+            vie.innerHTML = "il vous reste  3 vies";
         }
-         if (life === 2) {
-            vie.innerHTML = "attention que 2 vies";
+        if (life === 2) {
+            vie.innerHTML = "il vous reste  2 vies";
         }
         if (life === 1) {
-            vie.innerHTML = "attention que  1 vies";
+            vie.innerHTML = "il vous reste  1 vie";
+            window.onload = bonus();
         }
         if (life === 0) {
-            vie.innerHTML = "attention que 0 vies";
+            vie.innerHTML = "il vous reste 0 vies";
             zonejeu.removeChild(player);
             imlose();
-           
+
         }
 
     }
@@ -348,44 +381,108 @@ function PlayerEnnemi() {
     let enemi = document.querySelectorAll(".ennemi");
     for (let i = 0; i < enemi.length; i++) {
 
-        const playerTop = getProperty(player,"top");
-        const playerLeft = getProperty(player,"left");
+        const playerTop = getProperty(player, "top");
+        const playerLeft = getProperty(player, "left");
 
-        const ennemiTop = getProperty(enemi[i],"top");
-        const ennemiLeft = getProperty(enemi[i],"left");
+        const ennemiTop = getProperty(enemi[i], "top");
+        const ennemiLeft = getProperty(enemi[i], "left");
 
-       
+
 
         if (playerTop < ennemiTop + 50 && playerTop + 50 > ennemiTop &&
-            playerLeft < ennemiLeft + 50 && playerLeft + 50  > ennemiLeft ) {
+            playerLeft < ennemiLeft + 50 && playerLeft + 50 > ennemiLeft) {
 
-                life--;
-        if (life === 4) {
+            life--;
+            if (life === 4) {
 
-            //faire blinker le player pendant 3sec
-            
-            vie.innerHTML = "attention que 4 vies";
-            
-        }
-         if (life === 3) {
-            vie.innerHTML = "attention que 3 vies";
-        }
-         if (life === 2) {
-            vie.innerHTML = "attention que 2 vies";
-        }
-        if (life === 1) {
-            vie.innerHTML = "attention que  1 vies";
-        }
-        if (life === 0) {
-            vie.innerHTML = "attention que 0 vies";
-            zonejeu.removeChild(player);
-            imlose();
-           
-        }
-            
-            
+                
 
-           
+                vie.innerHTML = "il vous reste  4 vies";
+
+            }
+            if (life === 3) {
+                vie.innerHTML = "il vous reste  3 vies";
+            }
+            if (life === 2) {
+                vie.innerHTML = "il vous reste  2 vies";
+            }
+            if (life === 1) {
+
+                vie.innerHTML = "il vous reste   1 vie";
+                window.onload = bonus();
+            }
+            if (life === 0) {
+                vie.innerHTML = " il vous reste 0 vie";
+                zonejeu.removeChild(player);
+
+                imlose();
+
+            }
+
+
+
+
         }
     }
+
 }
+function bonus() {
+    let bonus = document.createElement('div');
+    bonus.setAttribute('class', 'bonus');
+    bonus.style.top = getRandomizer(500, 0) + "px";
+    bonus.style.left = getRandomizer(500, 0) + "px";
+    zonejeu.appendChild(bonus);
+
+}
+
+
+function takebonus() {
+    let bonuss = document.querySelector(".bonus");
+
+
+    const playerTop = getProperty(player, "top");
+    const playerLeft = getProperty(player, "left");
+
+    const bonussTop = getProperty(bonuss, "top");
+    const bonussLeft = getProperty(bonuss, "left");
+
+
+
+    if (playerTop < bonussTop + 50 && playerTop + 50 > bonussTop &&
+        playerLeft < bonussLeft + 50 && playerLeft + 50 > bonussLeft) {
+
+        life++;
+        vie.innerHTML = "attention que 2 vies";
+        zonejeu.removeChild(bonuss);
+
+
+
+
+
+    }
+
+
+}
+
+/*function creationObstacle (){
+    let a = getRandomizer(12, 0) * 50;
+    for(let i=0;i<3;i++){
+     let ob = document.createElement('div');
+     ob.setAttribute("class", "mur");
+        ob.style.top = a + "px";
+        ob.style.left = getRandomizer(12, 0)*50 + "px";
+        zonejeu.appendChild(ob);
+        
+    }
+    let b = getRandomizer(12, 0) * 50;
+    for(let i=0;i<3;i++){
+        let ob = document.createElement('div');
+        ob.setAttribute("class", "mur");
+           ob.style.top = b + "px";
+           ob.style.left = getRandomizer(12, 0)*50 + "px";
+           zonejeu.appendChild(ob);
+           
+       }
+       
+}
+window.addEventListener('load',creationObstacle);*/
